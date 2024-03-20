472c698054d6f2379b3d1b9fc37325e2eaf46ba8
MOELSAYEDOSMAN <dizzy10100961@gmail.com>
2024-03-20 03:29:37 +0200

    Add Modifications

--- Flask_api/app.py
+++ Flask_api/app.py
@@ -1,21 +1,38 @@
-from flask import Flask, request, jsonify
-from model.test_functions import check_interaction, get_interaction_name
+from flask import Flask, request, jsonify,make_response #make_response 
+##---------------------------------##
+from flask_cors import CORS
+##---------------------------------##
+
+from test_functions import check_interaction, get_interaction_name
+
 
 app = Flask(__name__)
+##---------------------------------##
+CORS(app)#enable Send From Api(Backend Asp)
+##---------------------------------##
 
-@app.route('/predict_interaction', methods=['POST'])
+#Change Name
+@app.route('/CheckInteraction', methods=['POST'])
 def predict_interaction():
     data = request.get_json()
     drug_1 = data.get('drug_1')
     drug_2 = data.get('drug_2')
     
     interaction_result = check_interaction(drug_1, drug_2)
-    interaction_name_index = get_interaction_name(drug_1, drug_2)
-    
-    return jsonify({
-        'interaction_result': interaction_result,
-        'interaction_name_index': interaction_name_index
+    ##---------------------------------##
+    if(interaction_result==0):
+        return jsonify({
+        'InteractionResult': False,
+        'InteractionNameIndex':-1
     })
+    else:
+        InteractionName=get_interaction_name(drug_1,drug_2)
+        return jsonify({
+        'InteractionResult': True,
+        'InteractionNameIndex':InteractionName
+    })
+    ##--------------------------------##
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
